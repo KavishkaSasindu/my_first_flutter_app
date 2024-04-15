5557445e9e4cfc9f844682e5e81c91a69e584c2b
KavishkaSasindu <sasindu0215@gmail.com>
2024-04-15 17:50:00 +0530

    updated by ksdev

--- lib/main.dart
+++ lib/main.dart
@@ -84,10 +84,21 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(
-                    height: 40,
+                    height: 20,
                   ),
                   Center(
                     child: Image.asset("assets/space2.png"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Center(
+                    child: Text(
+                      "Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum .",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
                   )
                 ],
               ),
